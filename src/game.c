e4a948b6ed60b3b6b6b2c140ddaf7458c599975f
aknoss <pedrorfig27@gmail.com>
2025-11-12 20:11:52 -0300

    chore: add level 1

--- src/game.c
+++ src/game.c
@@ -3,14 +3,15 @@
 
 static int finishScreen = 0;
 
+void DrawLevel1(void) {
+  DrawRectangle(0, GetScreenHeight() - 40, GetScreenWidth(), 40, BROWN);
+}
+
 void InitGameScreen(void) { finishScreen = 0; }
 
 void UpdateGameScreen(void) {}
 
-void DrawGameScreen(void) {
-  DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
-  DrawText("Hello World", 130, 220, 20, MAROON);
-}
+void DrawGameScreen(void) { DrawLevel1(); }
 
 void UnloadGameScreen(void) {
   // TODO: Unload GAMEPLAY screen variables here!
