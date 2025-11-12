35361048b205ac7de059d45dd2fd9d6d74f932d4
aknoss <pedrorfig27@gmail.com>
2025-11-12 19:38:44 -0300

    chore: remove unnecessary code

--- src/game.c
+++ src/game.c
@@ -1,25 +1,15 @@
 #include "game.h"
 #include "raylib.h"
 
-static int framesCounter = 0;
 static int finishScreen = 0;
 
-void InitGameScreen(void) {
-  framesCounter = 0;
-  finishScreen = 0;
-}
+void InitGameScreen(void) { finishScreen = 0; }
 
-void UpdateGameScreen(void) {
-  if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
-    finishScreen = 1;
-  }
-}
+void UpdateGameScreen(void) {}
 
 void DrawGameScreen(void) {
   DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
-  Vector2 pos = {20, 10};
-  DrawTextEx(font, "GAMEPLAY SCREEN", pos, font.baseSize * 3.0f, 4, MAROON);
-  DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+  DrawText("Hello World", 130, 220, 20, MAROON);
 }
 
 void UnloadGameScreen(void) {
